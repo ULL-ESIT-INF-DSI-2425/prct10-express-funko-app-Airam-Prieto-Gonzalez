0c47f832a64276fc6ed1ee2f479f6dc113d69f13
Airam Prieto <alu0101546377@ull.edu.es>
2025-04-23 10:26:51 +0000

    yes

--- src/modi/index.ts
+++ src/modi/index.ts
@@ -1,3 +1,32 @@
-export function my_func(): number {
-        return 1;
+import request from "request";
+
+/**
+ * 
+ * @param name 
+ * @param type 
+ * @param incantation 
+ * @returns 
+ */
+export function findSpells(name: string = '', type: string = '', incantation: string = '') {
+        return new Promise<request.Response>((resolve, reject) => {
+                let url: string = "http://wizard-world-api.herokuapp.com/Spells"
+                url += `?Name=${name}&Type=${type}&Incantation=${incantation}`
+                // console.log(requestUrl)
+                request({ url: url, json: true }, (error: Error, response) => {
+                        if (error) {
+                          reject(error);
+                        }
+                        else if (!response.body || response.body.length === 0) {
+                                reject('No spells found matching the criteria.');
+                        }
+                        else {
+                            resolve(response);
+                        }
+                });
+        })
+
 }
+
+// console.log(await findSpells('Levitation Charm', 'Charm', 'Wingardium Leviosa'))
+// console.log('\n\n=============================================================================\n\n')
+// console.log(await findSpells('Charm'))
--- test/modi/index.spec.ts
+++ test/modi/index.spec.ts
@@ -1,8 +1,26 @@
 import { describe, test, expect } from "vitest";
-import { my_func } from "../../src/modi/index.js";
+import { findSpells } from "../../src/modi/index.js";
 
 describe("Modi Tests", () => {
-  test("Unitary Test", () => {
-    expect(my_func()).toStrictEqual(1)
+  test("Unitary Test Yes", () => {
+    return findSpells('Levitation Charm', 'Charm', 'Wingardium Leviosa').then((data) => {
+      expect(data.body.length).toBe(1)
+    })
+  });
+  test("Unitary Property", () => {
+    return findSpells('Levitation Charm', 'Charm', 'Wingardium Leviosa').then((data) => {
+      expect(data.body[0].name).toBe("Levitation Charm") 
+    })
+  })
+  test("Type Property", () => {
+    return findSpells('', 'Charm', '').then((data) => {
+      expect(data.body.length).toBeGreaterThan(0)
+    })
+  })
+  test("Unitary Test No", () => {
+    return findSpells("Charm").catch((err) => {
+      expect(err).toBe('No spells found matching the criteria.')
+    })
+    
   });
 });
